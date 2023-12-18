b9a6369c321d69697d278d9a3e9eb270b5bc56f3
Knullack <63751877+Knullack@users.noreply.github.com>
2023-12-18 00:03:28 -0500

    formatting

--- andon_resolver.py
+++ andon_resolver.py
@@ -16,6 +16,7 @@ logging.basicConfig(level=logging.INFO)
 ANDON_SITE = "http://fc-andons-na.corp.amazon.com/HDC3?category=Pick&type=No+Scannable+Barcode"
 LOGIN_URL = "https://fcmenu-iad-regionalized.corp.amazon.com/login"
 
+
 def window(main_func):
     def resolve_andons_with_input():
         badge_number = badge_entry.get()
@@ -30,8 +31,8 @@ def window(main_func):
         window.iconbitmap(icon_path)
     except tk.TclError:
         None
-    window.resizable(False,False)
-    
+    window.resizable(False, False)
+
     # Calculate the screen width and height
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
@@ -62,10 +63,12 @@ def window(main_func):
     refresh_entry = tk.Entry(frame)
     refresh_entry.pack()
 
-    resolve_button = tk.Button(frame, text="Resolve Andons", command=resolve_andons_with_input)
-    resolve_button.pack(side=BOTTOM,padx=3, pady=3)
+    resolve_button = tk.Button(
+        frame, text="Resolve Andons", command=resolve_andons_with_input)
+    resolve_button.pack(side=BOTTOM, padx=3, pady=3)
 
     window.mainloop()
+
 
 def install_module(module_name):
     try:
@@ -73,10 +76,12 @@ def install_module(module_name):
     except ModuleNotFoundError:
         logging.info(f"{module_name} not found. Installing it now...")
         try:
-            subprocess.run([sys.executable, '-m', 'pip', 'install', module_name], check=True)
+            subprocess.run([sys.executable, '-m', 'pip',
+                           'install', module_name], check=True)
         except subprocess.CalledProcessError as e:
             logging.error(f"Error installing {module_name}: {e}")
             sys.exit(1)
+
 
 def navigate_to_website(driver, url, max_attempts=5):
     exception_count = 0
@@ -85,19 +90,23 @@ def navigate_to_website(driver, url, max_attempts=5):
             driver.get(url)
             return
         except WebDriverException as se:
-            logging.error(f'WebDriverException #{exception_count}: Error in loading URL {se.msg}\n')
+            logging.error(
+                f'WebDriverException #{exception_count}: Error in loading URL {se.msg}\n')
             s(0.3)
             exception_count += 1
     logging.error(f'{exception_count} WebDriverExceptions')
     sys.exit(1)
 
+
 def login(driver, badge):
     input_element = driver.find_element('xpath', '//*[@id="badgeBarcodeId"]')
     HELPER_type_and_click(input_element, badge)
 
+
 def HELPER_type_and_click(element, text_to_type):
     element.send_keys(text_to_type)
     element.send_keys(Keys.ENTER)
+
 
 def resolve_andons(driver, refresh_limit):
     for x in range(1, int(refresh_limit) + 1):
@@ -106,22 +115,29 @@ def resolve_andons(driver, refresh_limit):
         resolve_andon(driver)
         s(3)
 
+
 def select_andon(driver, x):
-    select_andon = driver.find_element('xpath', f'/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-table/div/div[3]/table/tbody/tr[{x}]/td[1]/awsui-radio-button/div/label/input')
+    select_andon = driver.find_element(
+        'xpath', f'/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-table/div/div[3]/table/tbody/tr[{x}]/td[1]/awsui-radio-button/div/label/input')
     driver.execute_script("arguments[0].scrollIntoView();", select_andon)
     s(0.8)
     select_andon.click()
     s(0.8)
-    view_andon = driver.find_element('xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-table/div/div[2]/div[1]/div[1]/span/div/div[2]/awsui-button[2]/button')
+    view_andon = driver.find_element(
+        'xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-table/div/div[2]/div[1]/div[1]/span/div/div[2]/awsui-button[2]/button')
     view_andon.click()
     s(0.8)
 
+
 def resolve_andon(driver):
-    resolve = driver.find_element('xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-modal/div[2]/div/div/div[2]/div/span/span/awsui-form/div/div[2]/span/span/awsui-form-section/div/div[2]/span/awsui-column-layout/div/span/div/awsui-form-field[4]/div/div/div/div/span/awsui-checkbox/label/input')
+    resolve = driver.find_element(
+        'xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-modal/div[2]/div/div/div[2]/div/span/span/awsui-form/div/div[2]/span/span/awsui-form-section/div/div[2]/span/awsui-column-layout/div/span/div/awsui-form-field[4]/div/div/div/div/span/awsui-checkbox/label/input')
     resolve.click()
     s(0.8)
-    save_changes = driver.find_element('xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-modal/div[2]/div/div/div[3]/span/div/div[2]/awsui-button[2]/button')
+    save_changes = driver.find_element(
+        'xpath', '/html/body/div/div/div/awsui-app-layout/div/main/div/div[2]/div/span/div/awsui-modal/div[2]/div/div/div[3]/span/div/div[2]/awsui-button[2]/button')
     save_changes.click()
+
 
 def main(badge_number, refresh_limit):
     install_module('selenium')
@@ -137,12 +153,14 @@ def main(badge_number, refresh_limit):
 
     navigate_to_website(driver, ANDON_SITE)
     login(driver, badge_number)
-    navigate_to_website(driver, ANDON_SITE)  # Redundant due to first driver navigation kicks out to FCMenu Login
+    # Redundant due to first driver navigation kicks out to FCMenu Login
+    navigate_to_website(driver, ANDON_SITE)
 
     resolve_andons(driver, refresh_limit)
 
     logging.info(f"\n\nAndons in session resolved: {refresh_limit}")
     driver.quit()
+
 
 if __name__ == "__main__":
     window(main)
